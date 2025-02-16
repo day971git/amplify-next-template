cfd25e62fa065d91e70bc57f92d9fe3bf0d00969
Danny Husada S <danny@Dannys-MacBook-Air.local>
2025-02-16 12:49:55 +0800

    first checkin2

--- app/page.tsx
+++ app/page.tsx
@@ -49,9 +49,7 @@ export default function App() {
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
+
       </div>
     </main>
   );
